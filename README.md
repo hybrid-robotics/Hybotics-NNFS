ac24688138ace257a2f4e3ebd6d21f2d296b9a6d
Dale Weber <hybotics.sd@gmail.com>
2023-11-29 10:41:51 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+I am working my way through the book Neural Networks From Scratch (https://nnfs.io).
+This is my code as I go through the book.
+------------------------------------------------------------------------------------
+# Hybotics-NNFS
---
+++ code/chapter04/activations.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# ReLU activation
+class ReLU:
+  # Forward pass
+  def forward(self, inputs):
+    # Calculate output values from inputs
+    output = np.maximum(0, inputs)
+
+    return output
+
+# Softmax activation
+class Softmax:
+  # Forward pass
+  def forward(self, inputs):
+    # Get unnormalized probabilities
+    exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+    # Normalize them for each sample
+    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+    return probabilities
